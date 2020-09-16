af287ee1b9ec7f42632835f2aff2a8fff814d53e
Guilherme Zago <contatogzii@gmail.com>
2020-09-16 10:00:07 -0300

    terminado e funcionando

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
+import CreateTransactionService from '../services/CreateTransactionService';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 // import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
@@ -10,6 +11,10 @@ const transactionRouter = Router();
 transactionRouter.get('/', (request, response) => {
   try {
     // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({ transactions, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -18,6 +23,18 @@ transactionRouter.get('/', (request, response) => {
 transactionRouter.post('/', (request, response) => {
   try {
     // TODO
+
+    const { title, value, type } = request.body;
+    
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository
+    );
+
+    const transaction = createTransaction.execute({
+      title,value,type
+    })
+
+    return response.json(transaction)
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
